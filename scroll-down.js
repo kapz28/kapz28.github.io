b8d81974743308b616bb3d753f71de42f931182f
Kapilan Satkunanathan <kasatkunanathan@gmail.com>
2018-10-07 22:35:37 -0400

    finalize navbar

--- scroll-down.js
+++ scroll-down.js
@@ -32,3 +32,20 @@ $(document).ready(function(){
     });
   });
 });
+
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('#navbar').css('background-color', '#303F9F');
+          $('#navbar').css('color', '#E8EAF6');
+       } else {
+          $('#navbar').css('background-color', 'transparent');
+          $('#navbar').css('color', '#303F9F');
+       }
+   });
+});
